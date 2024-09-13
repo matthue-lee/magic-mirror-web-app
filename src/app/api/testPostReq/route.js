d253f14eb489cf4fb053e36841630c5f5cf3c99c
Matthew Lee <tansenmatt@gmail.com>
2024-09-14 08:39:45 +1200

    fixed testpostreq

--- src/app/api/testPostReq/route.js
+++ src/app/api/testPostReq/route.js
@@ -9,3 +9,11 @@
 //         return NextResponse.json({ status: 'error', message: error.message }, { status: 500 });
 //     }
 // }
+
+
+export async function GET(request) {
+    return new Response(JSON.stringify({ message: "User info sent" }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
